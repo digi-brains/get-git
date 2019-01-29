ec07d6c2e9630ce316622ee44b5433af89a0973d
chris brown <chris@digitalbrains.io>
2019-01-29 12:32:57 -0800

    testing

--- src/GetGit.php
+++ src/GetGit.php
@@ -1,26 +1,16 @@
 <?php
-namespace GetGit;
+namespace DigiBrains;
 
-class GitFeed {
+class GetGit {
 
-    private $url;
     private $token;
-    private $user;
 
-    public function __construct( $git_url, $git_token, $git_user ) {
+    public function __construct( $git_token ) {
 
-        $this->url = $git_url;
         $this->token = $git_token;
-        $this->user = $git_user;
     }
 
-    public function get_feed {
-        '/repos?access_token='
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, '3');
-        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
-        curl_setopt($ch, CURLOPT_USERAGENT, $user);
+    public function get_feed() {
+        echo "It's working";
     }
+}
